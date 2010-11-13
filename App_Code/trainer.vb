db70598d1c9f4ac754767db2fc38a51e439e7e1d
JerredS <jmschmidt7@gmail.com>
2010-11-13 12:20:42 -0600

    Working on Train tab

---
+++ App_Code/trainer.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class trainer
+    Inherits Employee
+
+    Public Sub New(ByVal uname As String)
+        MyBase.New(uname)
+    End Sub
+
+    Public Sub New(ByVal uid As Integer)
+        MyBase.New(uid)
+    End Sub
+
+    Public Function completeTraining(ByVal training As Integer, ByVal initial_experience As Integer, ByVal status As Integer, ByVal did As Integer) As String
+        Dim sqlTraining As New SqlDataSource(System.Web.Configuration.WebConfigurationManager.ConnectionStrings("ProjectConnectionString").ToString(), "")
+        Dim level As Integer = trainingLevel(0)
+        Dim message As String = ""
+        Select Case level
+            Case 0
+                initial_experience = 0
+                message = "Training Requested"
+            Case 1
+                initial_experience = 1
+                message = "Training Requested"
+            Case 2
+                message = "No Training Available"
+                initial_experience = 2
+            Case 3
+                message = "Pending Training Request"
+                initial_experience = 3
+            Case 4
+                message = "Pending Training Request"
+                initial_experience = 4
+            Case 5
+                message = "Error"
+                initial_experience = 5
+        End Select
+
+        If initial_experience = 0 Or initial_experience = 1 Then
+            sqlTraining.UpdateCommand = ""
+            sqlTraining.UpdateParameters.Add("trainee_id", userid)
+            sqlTraining.UpdateParameters.Add("department_id", did)
+            sqlTraining.UpdateParameters.Add("date", 0)
+            sqlTraining.UpdateParameters.Add("final_experience", initial_experience)
+            sqlTraining.UpdateParameters.Add("status", status)
+
+            sqlTraining.Insert()
+        End If
+
+        Return message
+    End Function
+End Class
--- users/Train.aspx.vb
+++ users/Train.aspx.vb
@@ -2,4 +2,32 @@
 Partial Class users_Train
     Inherits System.Web.UI.Page
 
+    Dim m_trainer As trainer
+
+    Protected Sub completeTraining(ByVal sender As Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim _user As MembershipUser = Membership.GetUser(Page.User.Identity.Name)
+        m_trainer = New trainer(_user.UserName)
+        SqlTraining.SelectParameters("trainer_id").DefaultValue = m_trainer.userid
+    End Sub
+
+    Protected Sub ListViewTrain_ItemDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ListViewItemEventArgs) Handles ListViewTrain.ItemDataBound
+        Dim department_idLabel As Label = CType(e.Item.FindControl("department_idLabel"), Label)
+        Dim supervisor_idLabel As Label = CType(e.Item.FindControl("supervisor_idLabel"), Label)
+        Dim trainee_idLabel As Label = CType(e.Item.FindControl("trainee_idLabel"), Label)
+        Dim job_idLabel As Label = CType(e.Item.FindControl("job_idLabel"), Label)
+        Dim initial_experienceLabel As Label = CType(e.Item.FindControl("initial_experienceLabel"), Label)
+        Dim final_experienceLabel As Label = CType(e.Item.FindControl("final_experienceLabel"), Label)
+        Dim statusLabel As Label = CType(e.Item.FindControl("statusLabel"), Label)
+        Dim trainee As New Employee(CType(trainee_idLabel.Text, Integer))
+        Dim supervisor As New Employee(CType(supervisor_idLabel.Text, Integer))
+        job_idLabel.Text = trainee.getJobName(job_idLabel.Text)
+        statusLabel.Text = trainee.getStatus(statusLabel.Text)
+        trainee_idLabel.Text = trainee.first_name & " " & trainee.last_name
+        supervisor_idLabel.Text = supervisor.first_name & " " & supervisor.last_name
+        department_idLabel.Text = supervisor.getDepartmentName(department_idLabel.Text)
+    End Sub
 End Class
