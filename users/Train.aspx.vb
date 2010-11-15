60223227bc65e59081c5c06431c0111d134d03db
JerredS <jmschmidt7@gmail.com>
2010-11-14 21:47:48 -0600

    Added Training History Tab

--- users/Train.aspx.vb
+++ users/Train.aspx.vb
@@ -34,6 +34,7 @@ Partial Class users_Train
         Dim _user As MembershipUser = Membership.GetUser(Page.User.Identity.Name)
         m_trainer = New trainer(_user.UserName)
         SqlTraining.SelectParameters("trainer_id").DefaultValue = m_trainer.userid
+        SqlDataSource1.SelectParameters("trainer_id").DefaultValue = m_trainer.userid
     End Sub
 
     Protected Sub ListViewTrain_ItemDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ListViewItemEventArgs) Handles ListViewTrain.ItemDataBound
@@ -68,5 +69,38 @@ Partial Class users_Train
             hoursDropDown.Items.Add(dec.ToString("##0.00"))
         End While
 
+    End Sub
+
+    Protected Sub lnkComplete_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles lnkComplete.Click, LinkButton1.Click
+        MultiView1.ActiveViewIndex = 0
+    End Sub
+
+    Protected Sub lnkHistory_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles lnkHistory.Click, LinkButton2.Click
+        MultiView1.ActiveViewIndex = 1
+    End Sub
+
+    Protected Sub ListView1_ItemDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ListViewItemEventArgs) Handles ListView1.ItemDataBound
+        Dim department_idLabel As Label = CType(e.Item.FindControl("department_idLabel"), Label)
+        Dim supervisor_idLabel As Label = CType(e.Item.FindControl("supervisor_idLabel"), Label)
+        Dim trainee_idLabel As Label = CType(e.Item.FindControl("trainee_idLabel"), Label)
+        Dim job_idLabel As Label = CType(e.Item.FindControl("job_idLabel"), Label)
+        Dim initial_experienceLabel As Label = CType(e.Item.FindControl("initial_experienceLabel"), Label)
+        Dim final_experienceLabel As Label = CType(e.Item.FindControl("final_experienceLabel"), Label)
+        Dim statusLabel As Label = CType(e.Item.FindControl("statusLabel"), Label)
+        Dim hoursDropDown As DropDownList = CType(e.Item.FindControl("hoursDropDown"), DropDownList)
+        Dim datetextbox As TextBox = CType(e.Item.FindControl("datetextbox"), TextBox)
+        Dim trainee As New Employee()
+        If trainee_idLabel.Text <> "" Then
+            trainee = New Employee(CType(trainee_idLabel.Text, Integer))
+        End If
+        Dim supervisor As New Employee()
+        If supervisor_idLabel.Text <> "" Then
+            supervisor = New Employee(CType(supervisor_idLabel.Text, Integer))
+        End If
+        job_idLabel.Text = trainee.getJobName(job_idLabel.Text)
+        statusLabel.Text = trainee.getStatus(statusLabel.Text)
+        trainee_idLabel.Text = trainee.first_name & " " & trainee.last_name
+        supervisor_idLabel.Text = supervisor.first_name & " " & supervisor.last_name
+        department_idLabel.Text = supervisor.getDepartmentName(department_idLabel.Text)
     End Sub
 End Class
