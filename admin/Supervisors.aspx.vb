78314365ccfd4b0df7048be652ff9e439b36c1ce
Josh Cheek <josh.cheek@gmail.com>
2010-12-09 12:31:00 -0600

    currently broken: prevent users from adding same supervisor multiple times

--- admin/Supervisors.aspx.vb
+++ admin/Supervisors.aspx.vb
@@ -6,15 +6,18 @@ Partial Class users_MyTraining
     Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
   
         Dim sqlUser As New SqlDataSource(System.Web.Configuration.WebConfigurationManager.ConnectionStrings("ProjectConnectionString").ToString(), "")
-        sqlUser.InsertCommand = "INSERT INTO supervisee_supervisor(supervisee_id,supervisor_id) VALUES (@supervisee_id,@supervisor_id)"
 
+        sqlUser.SelectCommand = "SELECT CONT(*) FROM supervisee_supervisor WHERE (supervisee_id = @supervisee_id) AND (supervisor_id = @supervisor_id)"
+        sqlUser.SelectParameters.Add("supervisor_id", DropDownList1.SelectedValue)
+        sqlUser.SelectParameters.Add("supervisee_id", DropDownList2.SelectedValue)
 
-        sqlUser.InsertParameters.Add("supervisor_id", DropDownList1.SelectedValue)
-        sqlUser.InsertParameters.Add("supervisee_id", DropDownList2.SelectedValue)
+        If sqlUser.Select() = "0" Then
+            sqlUser.InsertCommand = "INSERT INTO supervisee_supervisor(supervisee_id,supervisor_id) VALUES (@supervisee_id,@supervisor_id)"
+            sqlUser.InsertParameters.Add("supervisor_id", DropDownList1.SelectedValue)
+            sqlUser.InsertParameters.Add("supervisee_id", DropDownList2.SelectedValue)
+            sqlUser.Insert()
+        End If
 
-   
-        sqlUser.Insert()
         ListView1.DataBind()
-
     End Sub
 End Class
